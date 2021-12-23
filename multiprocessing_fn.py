b6b4361f0779a81390b52dd0c503b68057c69cfa
Mbugz1000 <44344898+Mbugz1000@users.noreply.github.com>
2021-12-23 18:07:29 +0300

    Progress: - Updated notebook and multiprocessing_fn.py

--- multiprocessing_fn.py
+++ multiprocessing_fn.py
@@ -1,10 +1,21 @@
-import nltk
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 
+
 def clean_body(body):
+    """
+    This method cleans the body text of the articles. It performs the following tasks:
+        1. Lowers the capitalization
+        2. Removes punctuation
+        3. Tokenizes the text
+        4. Removes stop words
+        5. Lemmatizes the tokens
+    
+    :param body: Body text from the IBM website
+    :return: Clean tokenized body text
+    """
     body = body.lower()
     punctuation_removed = re.sub(r"[^a-zA-Z0-9]", " ", body)
     tokenized = word_tokenize(punctuation_removed)
